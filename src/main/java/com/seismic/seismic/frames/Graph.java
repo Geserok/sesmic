35f8566b848e77fb21d36a3beb5b4c992303eb2a
Andrei Kudarenko <Andrei_Kudarenko@epam.com>
2019-08-30 20:27:30 +0300

    add Dataimporter and Coordinates

--- src/main/java/com/seismic/seismic/frames/Graph.java
+++ src/main/java/com/seismic/seismic/frames/Graph.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -33,25 +34,23 @@ public class Graph {
 
     public void addSeriesToChart(XYChart chart) {
         List<Double> yCoordinates = coordinates.getYCoordinates();
-        List<Double> xFirstCoordinates = coordinates.getXThirdCoordinates();
+        List<Double> xFirstCoordinates = coordinates.getXFirstCoordinates();
         List<Double> doubles = prepareCoordinates(xFirstCoordinates);
         chart.addSeries("1", doubles, yCoordinates).setMarker(SeriesMarkers.NONE);
 
     }
 
     private List<Double> prepareCoordinates(List<Double> xCoordinates) {
-        int size = xCoordinates.size();
-        List<Double> sortedCoordinates = new ArrayList(xCoordinates);
-        sortedCoordinates.sort(Double::compareTo);
-        Double maxBound = sortedCoordinates.get(size - 1);
-        Double minBound = sortedCoordinates.get(0);
-        if (minBound > 0) {
-            return xCoordinates.stream().map(a -> a - minBound).collect(Collectors.toList());
-        } else if (minBound < 0) {
-            return xCoordinates.stream().map(a -> a - minBound).collect(Collectors.toList());
-        } else {
-            return xCoordinates;
+        Optional<Double> minBound = xCoordinates.stream().min(Double::compareTo);
+        List<Double> collect = new ArrayList<>();
+        if (minBound.get() > 0) {
+            collect = xCoordinates.stream().map(a -> a - minBound.get()).collect(Collectors.toList());
+        } else if (minBound.get() < 0) {
+            collect = xCoordinates.stream().map(a -> a - minBound.get()).collect(Collectors.toList());
         }
+        Optional<Double> maxBound = collect.stream().max(Double::compareTo);
+        double scale = 100 / maxBound.get();
+        return collect.stream().map(a -> a * scale).collect(Collectors.toList());
     }
 }
 
